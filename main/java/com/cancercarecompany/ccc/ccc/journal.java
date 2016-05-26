1372322660025db2240886f8641060dcd25d620d
christianbarlof <christian@cancercarecompany.com>
2016-05-26 12:04:42 +0200

    chb26-5

---
+++ main/java/com/cancercarecompany/ccc/ccc/journal.java
@@ -0,0 +1,24 @@
+package com.cancercarecompany.ccc.ccc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class journal extends AppCompatActivity {
+
+    String lbl_datum;
+    TextView header;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal);
+
+//        header.findViewById(R.id.diary_header);
+
+//        lbl_datum = "Vald Datum";
+//        System.out.println("Nu börjas det");
+//        System.out.println(header);
+
+    }
+}
